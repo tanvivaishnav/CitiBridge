94d971014f65108df848f24de735431d75732aa8
Swapnali-gunjawate <55696423+Swapnali-gunjawate@users.noreply.github.com>
2020-04-03 02:32:23 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {HttpClient ,HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = ' Agnew ';
+  array =[];
+  fullname:String ='';
+  pass:String='';
+
+ 
+
+  }
+
+
+ 
+
